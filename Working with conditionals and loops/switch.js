0625a1336dbb98c61817afe045b653f44249456f
Min-Thaway-Htut <minthwayhtut568@gmail.com>
2024-06-02 17:09:21 +0200

    First release

---
+++ Working with conditionals and loops/switch.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 10; i++){
+    switch(i){
+        case 1:
+        console.log("Gold medal");
+        break;
+        case 2:
+        console.log("Silver medal");
+        break;
+        case 3:
+        console.log("Bronze medal");
+        break;
+        case i:
+        console.log(i)
+        break;
+    }
+}
