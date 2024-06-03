9db91f4977deacf07abe9f686ba1483cad9baf19
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-06-03 21:37:04 +0200

    Adding letterFinder exercise

---
+++ calling-functions/array.js
@@ -0,0 +1,9 @@
+function displayingArrays(array){
+    for(let i = 0; i < array.length; i++){
+        console.log(i, array[i])
+    }
+}
+
+array = ["Apple", "Orange", "Mango", "Banana", "Strawberry"]
+displayingArrays(array);
+
