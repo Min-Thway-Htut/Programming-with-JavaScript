bd77bceede712cb2f79922a2d24114fcbeeb1d8e
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-05 00:11:15 +0200

    Update

---
+++ strings/string.js
@@ -0,0 +1,12 @@
+var footballPlayers = ['Ronaldo', 'Messi', 'Pele'];
+
+
+console.log(footballPlayers.length);
+
+console.log(footballPlayers[0]);
+
+console.log(footballPlayers[1]);
+
+for (var i = 0; i < footballPlayers.length; i++){
+    console.log(footballPlayers[i]);
+}
