4aab437ca1aa2858f245c7728b9770164e7fae7a
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-07 23:48:39 +0200

    Type of

---
+++ TypeOf/typeOf.js
@@ -0,0 +1,16 @@
+//var test = typeof("What is your name?")
+
+//var test = typeof(10);
+
+//var test = typeof(3.14);
+
+//var test = typeof(true);
+
+//var test = typeof(1 < 2);
+
+//var test = typeof([1,2,3])
+
+//var test = typeof({firstProperty: 1});
+
+var test = typeof(function abc(){ console.log('abc')});
+console.log(test);
