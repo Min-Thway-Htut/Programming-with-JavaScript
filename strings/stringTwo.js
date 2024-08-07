4a705781fc81d6c018e03fa9d151b6a3bf8fafa5
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-07 23:25:04 +0200

    Array, Push, Pop

---
+++ strings/stringTwo.js
@@ -0,0 +1,10 @@
+var clothes = [];
+clothes.push("Adidas");
+clothes.push("Nike");
+clothes.push("Puma");
+clothes.push("Fila");
+clothes.push("Erke");
+
+clothes.pop();
+clothes.push("CK");
+console.log(clothes);
