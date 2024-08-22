2d1d271df464844e22476cc509e38daeb7f0e7d8
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-22 16:37:50 +0200

    Template literals

---
+++ template_literals/test.js
@@ -0,0 +1,23 @@
+let greet = "Hello";
+let place = "world";
+console.log(`${greet} ${place}!`)
+console.log(greet,place);
+
+console.log(`${1 + 1 + 1 + 1} stars`);
+
+let noMultiLine = "No multi-line strings in ES5";
+console.log("Did you know? ", noMultiLine);
+
+let multiLine = `
+    Using ES6
+    backticks,
+    multi-line
+    strings are
+    possible!
+`
+console.log(multiLine);
+
+let first = "My footballing idol is Cristiano Ronaldo!";
+let second = "He is the GOAT!";
+
+console.log(`${first} Additionally, ${second}`);
