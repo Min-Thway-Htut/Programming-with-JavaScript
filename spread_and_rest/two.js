e10b25826a91a1f3943bf75e2cae37c1c8f2984c
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-09-05 13:25:39 +0200

    add javaScript

---
+++ spread_and_rest/two.js
@@ -0,0 +1,12 @@
+const meal = ["soup", "steak", "ice cream"]
+const [starter] = meal;
+console.log(starter);
+
+let set = new Set();
+set.add(1);
+set.add(2);
+set.add(3);
+set.add(2);
+set.add(1);
+
+console.log(set)
